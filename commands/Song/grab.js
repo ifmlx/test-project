28d5ed9e3bc8bae4dfcc2923dabd3d486b68cfe9
JANO <77622056+Devjano@users.noreply.github.com>
2022-01-09 18:26:14 -0800

    Create grab.js

---
+++ commands/Song/grab.js
@@ -0,0 +1,97 @@
+const { MessageEmbed, Message } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+const { check_if_dj } = require("../../handlers/functions")
+module.exports = {
+	name: "grab", //the command name for the Slash Command
+	category: "Song",
+	usage: "grab",
+	aliases: ["take", "steal"],
+	description: "Jumps to a specific Position in the Song", //the command description for Slash Command Overview
+	cooldown: 10,
+	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+	run: async (client, message, args) => {
+		try {
+			//things u can directly access in an interaction!
+			const {
+				member,
+				channelId,
+				guildId,
+				applicationId,
+				commandName,
+				deferred,
+				replied,
+				ephemeral,
+				options,
+				id,
+				createdTimestamp
+			} = message;
+			const {
+				guild
+			} = member;
+			const {
+				channel
+			} = member.voice;
+			if (!channel) return message.reply({
+				embeds: [
+					new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Please join ${guild.me.voice.channel ? "my" : "a"} VoiceChannel First!**`)
+				],
+
+			})
+			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
+				return message.reply({
+					embeds: [new MessageEmbed()
+						.setColor(ee.wrongcolor)
+						.setFooter(ee.footertext, ee.footericon)
+						.setTitle(`${client.allEmojis.x} **Join my Voice Channel!**`)
+						.setDescription(`<#${guild.me.voice.channel.id}>`)
+					],
+				});
+			}
+			try {
+				let newQueue = client.distube.getQueue(guildId);
+				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return message.reply({
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **I am nothing Playing right now!**`)
+					],
+
+				})
+				let newTrack = newQueue.songs[0];
+				member.send({
+					embeds: [
+						new MessageEmbed().setColor(ee.color)
+						.setTitle(newTrack.name)
+						.setURL(newTrack.url)
+						.setURL(newTrack.url)
+						.addField(`Duration:`, `\`${newQueue.formattedCurrentTime} / ${newTrack.formattedDuration}\``)
+						.addField(`Download Song:`, `[Click here](${newTrack.streamURL})`)
+						.setThumbnail(`https://img.youtube.com/vi/${newTrack.id}/mqdefault.jpg`)
+						.setFooter(`Played in: ${guild.name}`, guild.iconURL({dynamic: true}))
+					]
+				}).then(() => {
+					message.reply({
+						content: `📪 **Grabbed! Check your Dm!**`,
+					})
+				}).catch(() => {
+					message.reply({
+						content: `${client.allEmojis.x} **I can't dm you!**`,
+					})
+				})
+			} catch (e) {
+				console.log(e.stack ? e.stack : e)
+				message.reply({
+					content: `${client.allEmojis.x} | Error: `,
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor)
+						.setDescription(`\`\`\`${e}\`\`\``)
+					],
+
+				})
+			}
+		} catch (e) {
+			console.log(String(e.stack).bgRed)
+		}
+	}
+}
