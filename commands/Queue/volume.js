f44c26cf4634ed97e86399ac47dc254eb2c506ba
JANO <77622056+Devjano@users.noreply.github.com>
2022-01-09 18:21:26 -0800

    Create volume.js

---
+++ commands/Queue/volume.js
@@ -0,0 +1,113 @@
+const { MessageEmbed, Message } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+const { check_if_dj } = require("../../handlers/functions")
+module.exports = {
+	name: "volume", //the command name for the Slash Command
+
+	category: "Queue",
+	aliases: ["vol"],
+	usage: "volume <newVolume>",
+
+	description: "Adjusts the Volume of the Music", //the command description for Slash Command Overview
+	cooldown: 5,
+	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+
+	run: async (client, message, args) => {
+		try {
+			//things u can directly access in an interaction!
+			const {
+				member,
+				channelId,
+				guildId,
+				applicationId,
+				commandName,
+				deferred,
+				replied,
+				ephemeral,
+				options,
+				id,
+				createdTimestamp
+			} = message;
+			const {
+				guild
+			} = member;
+			const {
+				channel
+			} = member.voice;
+			if (!channel) return message.reply({
+				embeds: [
+					new MessageEmbed().setColor(ee.wrongcolor).setDescription(`${client.allEmojis.x} Please join ${guild.me.voice.channel ? "my" : "a"} VoiceChannel First!`)
+				],
+
+			})
+			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
+				return message.reply({
+					embeds: [new MessageEmbed()
+						.setColor(ee.wrongcolor)
+						.setFooter(ee.footertext, ee.footericon)
+						.setTitle(`${client.allEmojis.x} Join my Voice Channel!`)
+						.setDescription(`<#${guild.me.voice.channel.id}>`)
+					],
+				});
+			}
+			try {
+				let newQueue = client.distube.getQueue(guildId);
+				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return message.reply({
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor).setDescription(`${client.allEmojis.x} I am nothing Playing right now!`)
+					],
+
+				})
+				if (check_if_dj(client, member, newQueue.songs[0])) {
+					return message.reply({
+						embeds: [new MessageEmbed()
+							.setColor(ee.wrongcolor)
+							.setFooter(ee.footertext, ee.footericon)
+							.setTitle(`${client.allEmojis.x} **You are not a DJ and not the Song Requester!**`)
+							.setDescription(`**DJ-ROLES:**\n> ${check_if_dj(client, member, newQueue.songs[0])}`)
+						],
+					});
+				}
+				if (!args[0]) {
+					return message.reply({
+						embeds: [new MessageEmbed()
+							.setColor(ee.wrongcolor)
+							.setFooter(ee.footertext, ee.footericon)
+							.setTitle(`🔊 Volume Manager`)
+							.setDescription(`**Now:** \`%${newQueue.volume}\`\n**Usage:** \`${client.settings.get(message.guild.id, "prefix")}volume <Percentage>\``)
+						],
+					});
+				}
+				let volume = Number(args[0])
+				if (volume > 150 || volume < 0) return message.reply({
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor).setDescription(`The Volume must be between **0** and **150**`)
+					],
+
+				})
+				await newQueue.setVolume(volume);
+				message.reply({
+					embeds: [new MessageEmbed()
+					  .setColor(ee.color)
+					  .setDescription(`🎚 Changed the Volume to **${volume}**`)
+                                 ]
+				})
+			} catch (e) {
+				console.log(e.stack ? e.stack : e)
+				message.reply({
+					content: `${client.allEmojis.x} Error: `,
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor)
+						.setDescription(`\`\`\`${e}\`\`\``)
+					],
+
+				})
+			}
+		} catch (e) {
+			console.log(String(e.stack).bgRed)
+		}
+	}
+}
